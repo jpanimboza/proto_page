7734ee625ed1400c7f7c0f3afca4f49de3fe4429
jpanimboza <61572277+jpanimboza@users.noreply.github.com>
2023-10-08 00:07:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,22 +8,14 @@ map.addLayer(osmLayer);
 
 $.getJSON('map_airport.geojson',function(json){
 	var geoLayer = L.geoJson(json).addTo(map);
-    /*L.geoJson(data,{
-      pointToLayer: function(feature,layer){
-        var marker = L.marker(layer,{icon: Icon});
-        marker.bindPopup('<b>AIRPORT NAME : </b>'+feature.properties.name+' ('+feature.properties.abbrev+')<br>\
-        <b>TYPE OF AIPORT : </b>'+feature.properties.type+'<br>\
-        <a href="'+feature.properties.wikipedia+'">Wikipedia</a>').addTo(map);
-        marker.addTo(map)
-        return marker;
-      }
-    },
-             );*/
+    	L.geoJson(json,{
+      		pointToLayer: function(feature,layer){
+        		var marker = L.marker(layer,/*{icon: Icon}*/);
+        		marker.bindPopup('<b>AIRPORT NAME : </b>'+feature.properties.name+' ('+feature.properties.abbrev+')<br>\
+        		<b>TYPE OF AIPORT : </b>'+feature.properties.type+'<br>\
+        		<a href="'+feature.properties.wikipedia+'">Wikipedia</a>').addTo(map);
+       	 		marker.addTo(map)
+        		return marker;
+      		}
+    	});
 });
-var Icon = new L.Icon({
-  	iconUrl: 'https://cdn-icons-png.flaticon.com/512/0/614.png',
-  	iconSize: [40,40],
-  	iconAnchor: [12, 41],
-  	popupAnchor: [1, -34],
-  	shadowSize: [41, 41]
-	});	

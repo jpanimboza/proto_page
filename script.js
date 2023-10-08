db83d3c3586c8c8e0364c42df0d8475769c3b18e
jpanimboza <61572277+jpanimboza@users.noreply.github.com>
2023-10-07 22:38:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-// Función que se ejecuta una vez que se carga el DOM
-document.addEventListener("DOMContentLoaded", function() {
-    // Crea un mapa y establece sus coordenadas iniciales
-    var mymap = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([37.88437176085360, -4.779524803161621], 14);
 
-    // Agrega una capa de mapa base de OpenStreetMap
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(mymap);
-
-    // Agrega un marcador al mapa
-    var marker = L.marker([51.5, -0.09]).addTo(mymap);
-
-    // Agrega un pop-up al marcador
-    marker.bindPopup("<b>¡Hola!</b><br>Esto es un marcador.").openPopup();
-});
+// Capas base
+var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap<\/a> contributors'
+}).addTo(map);

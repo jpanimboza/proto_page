55a520176c8aac77f2978835c123fed927f9cdfe
jpanimboza <61572277+jpanimboza@users.noreply.github.com>
2023-10-07 23:28:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,20 @@ map.addLayer(osmLayer);
 $.getJSON("https://jpanimboza.github.io/proto_docs/map_airport.geojson",function(data){
     L.geoJson(data,{
       pointToLayer: function(feature,layer){
-        var marker = L.marker(layer,/* {icon: Icon}*/);
-/*        marker.bindPopup('<b>AIRPORT NAME : </b>'+feature.properties.name+' ('+feature.properties.abbrev+')<br>\
+        var marker = L.marker(layer,{icon: Icon});
+        marker.bindPopup('<b>AIRPORT NAME : </b>'+feature.properties.name+' ('+feature.properties.abbrev+')<br>\
         <b>TYPE OF AIPORT : </b>'+feature.properties.type+'<br>\
-        <a href="'+feature.properties.wikipedia+'">Wikipedia</a>').addTo(map);*/
+        <a href="'+feature.properties.wikipedia+'">Wikipedia</a>').addTo(map);
         marker.addTo(map)
         return marker;
       }
     }, 
              );
 });
+var Icon = new L.Icon({
+  	iconUrl: 'https://cdn-icons-png.flaticon.com/512/0/614.png',
+  	iconSize: [40,40],
+  	iconAnchor: [12, 41],
+  	popupAnchor: [1, -34],
+  	shadowSize: [41, 41]
+	});	

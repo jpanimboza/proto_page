e7048bd7dafc20fe5407e267d4fce5a60476dda9
jpanimboza <61572277+jpanimboza@users.noreply.github.com>
2023-10-08 00:26:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-var map = L.map('map').setView([48.833, 2.333],5); 
 
-var osmLayer = L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: 'Jonathan'
-  }).addTo(map);
-    
-map.addLayer(osmLayer);
+var map = L.map('map').setView([37.344,-4.548], 8); // Andalucía
 
-var museosand = L.geoJson(null);
+// Capas base
+var cartodbLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
+}).addTo(map);
+
+// Función para ventana de datos al hacer click sobre el punto
+
+function popup(feature, layer) { 
+	if (feature.properties && feature.properties.nomb_mus) 
+	{ 
+		layer.bindPopup( "<strong>" + feature.properties.nomb_mus + "</strong><br/>" + feature.properties.municipio + ". " + feature.properties.provincia ); 
+	} 
+}
+
+// Cargar GeoJSON desde un archivo externo
+
+// $.getJSON("museos_wfsdera.geojson", function (data) {
+// var museosand = new L.geoJson(data, {
+// 	onEachFeature: popup
+// 	}).addTo(map);
+// })
+
+
+var museosand = L.geoJson(null, {
+	onEachFeature: popup
+});
 
 $.getJSON("map_airport.geojson", function (data) {
 	museosand.addData(data);

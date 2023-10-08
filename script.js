2323edf39090bf9e2ed9f5791ab3eda2edb23220
jpanimboza <61572277+jpanimboza@users.noreply.github.com>
2023-10-07 23:22:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-var map = L.map('map').setView([37.88437176085360, -4.779524803161621], 14);
+var map = L.map('map').setView([48.833, 2.333],5); 
 
-// Capas base
-var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap<\/a> contributors'
-}).addTo(map);
+var osmLayer = L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Emilie Locquette'
+  }).addTo(map);
+    
+map.addLayer(osmLayer);
+
+$.getJSON("https://jpanimboza.github.io/proto_docs/map_airport.geojson",function(data){
+    L.geoJson(data,{
+      pointToLayer: function(feature,layer){
+        var marker = L.marker(layer,/* {icon: Icon}*/);
+/*        marker.bindPopup('<b>AIRPORT NAME : </b>'+feature.properties.name+' ('+feature.properties.abbrev+')<br>\
+        <b>TYPE OF AIPORT : </b>'+feature.properties.type+'<br>\
+        <a href="'+feature.properties.wikipedia+'">Wikipedia</a>').addTo(map);*/
+        marker.addTo(map)
+        return marker;
+      }
+    }, 
+             );
+});
